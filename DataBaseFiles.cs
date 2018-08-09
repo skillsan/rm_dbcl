4860050e9ad52945530bcefe6637fab4043b536f
Julien Hohweiller <julien.hohweiller@laposte.net>
2018-08-09 11:41:07 +0200

    1er Ajout : Classe Item, gestion des objets. Simple lecture.

---
+++ DataBaseFiles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace RM_DataBase_classes
+{
+    public class DataBaseFiles
+    {
+        private DirectoryInfo dataProjectDirInfo;
+
+        public static string dataProjectItemsFile = "Items.json";
+        private List<Rm_DataBase_Item> JsonItem;
+        public static string dataProjectWeaponsFile = "Weapons.json";
+        private JsonReader JsonWeapons;
+        public static string dataProjectArmorsFile = "Armors.json";
+        private JsonReader JsonArmors;
+        public static string dataProjectEnemiesFile = "Enemies.json";
+        private JsonReader JsonEnemies;
+
+        public static string dataProjectActorsFile = "";
+        public static string dataProjectAnimationsFile = "";
+        public static string dataProjectClassesFile = "";
+        public static string dataProjecCommonEventsFile = "";
+        public static string dataProjectSkillsFile = "";
+        public static string dataProjectStatesFile = "";
+        public static string dataProjectSystemFile = "";
+        public static string dataProjectTilesetsFile = "";
+        public static string dataProjectTroopsFile = "";
+
+        public DataBaseFiles(string dirname)
+        {
+            this.dataProjectDirInfo = new DirectoryInfo(dirname);
+
+            if (this.dataProjectDirInfo.Exists)
+            {
+                List<string> arrayFiles = new List<string>(Directory.GetFiles(this.dataProjectDirInfo.ToString()));
+
+                if (arrayFiles.Contains(this.dataProjectDirInfo.ToString() + "\\" + DataBaseFiles.dataProjectItemsFile))
+                {
+                    this.JsonItem = JsonConvert.DeserializeObject<List<Rm_DataBase_Item>>(File.ReadAllText(this.dataProjectDirInfo.ToString() + "\\" + DataBaseFiles.dataProjectItemsFile));
+                }
+            }
+        }
+
+        public List<Rm_DataBase_Item> Items { get { return this.JsonItem; } }
+    }
+}
---
+++ Rm_DataBase_Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM_DataBase_classes
+{
+    public class Rm_DataBase_Item
+
+    {
+        public int id;
+        public int animationId;
+        public bool consumable;
+        //public string damage;
+        public string description;
+        //public string[] effects;
+        public int hitType;
+        public int iconIndex;
+        public int itypeId;
+        public string name;
+        public string note;
+        public int occasion;
+        public int price;
+        public int repeats;
+        public int scope;
+        public int speed;
+        public int successRate;
+        public int tpGain;
+
+        public Rm_DataBase_Item()
+        {
+
+        }
+    }
+}
